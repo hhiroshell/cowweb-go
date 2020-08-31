6c5a1fac3a94a58b59e6a89d00c47f6351b2f552
Hiroshi Hayakawa <hirhayak@yahoo-corp.jp>
2020-08-31 16:21:10 +0900

    Random cow styles / Specify moosage string in query parameters

--- cmd/cowweb/main.go
+++ cmd/cowweb/main.go
@@ -3,18 +3,59 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 
 	cowsay "github.com/Code-Hex/Neo-cowsay"
 )
 
+const (
+	defaultMoosage = "Moo!"
+)
+
+var cows []string
+
+func init() {
+	var infelicities = [3]string{"head-in", "telebears", "sodomized"}
+	contains := func(s string) bool {
+		for _, inf := range infelicities {
+			if s == inf {
+				return true
+			}
+		}
+		return false
+	}
+	for _, c := range cowsay.Cows() {
+		if contains(c) {
+			continue
+		}
+		cows = append(cows, c)
+	}
+}
+
+func randomCowType() cowsay.Option {
+	return cowsay.Type(cows[rand.Intn(len(cows))])
+}
+
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		say, err := cowsay.Say(
-			cowsay.Phrase("Hello"),
-			cowsay.Type("default"),
-			cowsay.BallonWidth(40),
-		)
+	http.HandleFunc("/say", func(w http.ResponseWriter, r *http.Request) {
+		moo := r.URL.Query().Get("m")
+		if moo == "" {
+			moo = defaultMoosage
+		}
+		say, err := cowsay.Say(cowsay.Phrase(moo), randomCowType())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, say)
+	})
+
+	http.HandleFunc("/think", func(w http.ResponseWriter, r *http.Request) {
+		moo := r.URL.Query().Get("m")
+		if moo == "" {
+			moo = defaultMoosage
+		}
+		say, err := cowsay.Say(cowsay.Phrase(moo), randomCowType(),	cowsay.Thinking())
 		if err != nil {
 			panic(err)
 		}
@@ -23,3 +64,4 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
