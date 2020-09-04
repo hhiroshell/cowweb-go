885c6c7b8f5f690e51b169c136e933bcf36f73d9
Hiroshi Hayakawa <hhiroshell@gmail.com>
2020-09-05 01:19:07 +0900

    Real CPU load

--- cmd/cowweb/main.go
+++ cmd/cowweb/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	cowsay "github.com/Code-Hex/Neo-cowsay"
 )
 
 const (
 	defaultMoosage = "Moo!"
-	defaultWaitMillisecond = 5
-	envCowwebWaitMillisecondKey = "COWWEB_WAIT_MILLISECOND"
+	//defaultWaitMillisecond = 5
+	//envCowwebWaitMillisecondKey = "COWWEB_WAIT_MILLISECOND"
+	defaultLoad = 1000
+	envCowwebLoadKey = "COWWEB_LOAD"
 )
 
 var cows []string
+var load int
 
 func init() {
 	var infelicities = [3]string{"head-in", "telebears", "sodomized"}
@@ -37,21 +38,38 @@ func init() {
 		}
 		cows = append(cows, c)
 	}
+
+	lenv := os.Getenv(envCowwebLoadKey)
+	l, err := strconv.ParseInt(lenv, 10, 0)
+	if err != nil {
+		l = defaultLoad
+	}
+	load = int(l)
 }
 
 func randomCowType() cowsay.Option {
 	return cowsay.Type(cows[rand.Intn(len(cows))])
 }
 
-func waitWithLoad(ctx context.Context) {
-	for {
-		select {
-		case <- ctx.Done():
-			return
-		default:
+func randomCowTypeWithLoad() cowsay.Option {
+	randInts := []int{}
+	for i := 0; i < load; i++ {
+		for j := 0; j < load; j++ {
+			randInts = append(randInts, rand.Intn(len(cows)))
 		}
 	}
+	return cowsay.Type(cows[randInts[rand.Intn(load)]])
 }
+
+//func waitWithLoad(ctx context.Context) {
+//	for {
+//		select {
+//		case <- ctx.Done():
+//			return
+//		default:
+//		}
+//	}
+//}
 
 func main() {
 	http.HandleFunc("/say", func(w http.ResponseWriter, r *http.Request) {
@@ -83,19 +101,19 @@ func main() {
 		if moo == "" {
 			moo = defaultMoosage
 		}
-		say, err := cowsay.Say(cowsay.Phrase(moo), randomCowType(),	cowsay.Thinking())
+		say, err := cowsay.Say(cowsay.Phrase(moo), randomCowTypeWithLoad())
 		if err != nil {
 			panic(err)
 		}
 		fmt.Fprintln(w, say)
 
-		wenv := os.Getenv(envCowwebWaitMillisecondKey)
-		wms, err := strconv.ParseInt(wenv, 10, 0)
-		if err != nil {
-			wms = defaultWaitMillisecond
-		}
-		ctx, _ := context.WithTimeout(context.Background(), time.Duration(wms) * time.Millisecond)
-		waitWithLoad(ctx)
+		//wenv := os.Getenv(envCowwebWaitMillisecondKey)
+		//wms, err := strconv.ParseInt(wenv, 10, 0)
+		//if err != nil {
+		//	wms = defaultWaitMillisecond
+		//}
+		//ctx, _ := context.WithTimeout(context.Background(), time.Duration(wms) * time.Millisecond)
+		//waitWithLoad(ctx)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
