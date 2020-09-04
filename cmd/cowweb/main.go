61f6b70119fab7d767a3cf65ba1c24462c99d55e
Hiroshi Hayakawa <hhiroshell@gmail.com>
2020-09-05 00:01:20 +0900

    Add wait with cpu loading endpoint

--- cmd/cowweb/main.go
+++ cmd/cowweb/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	cowsay "github.com/Code-Hex/Neo-cowsay"
 )
 
 const (
 	defaultMoosage = "Moo!"
+	envCowwebWaitMillisecondKey = "COWWEB_WAIT_MILLISECOND"
 )
 
 var cows []string
@@ -37,6 +42,16 @@ func randomCowType() cowsay.Option {
 	return cowsay.Type(cows[rand.Intn(len(cows))])
 }
 
+func waitWithLoad(ctx context.Context) {
+	for {
+		select {
+		case <- ctx.Done():
+			return
+		default:
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/say", func(w http.ResponseWriter, r *http.Request) {
 		moo := r.URL.Query().Get("m")
@@ -60,6 +75,26 @@ func main() {
 			panic(err)
 		}
 		fmt.Fprintln(w, say)
+	})
+
+	http.HandleFunc("/load", func(w http.ResponseWriter, r *http.Request) {
+		moo := r.URL.Query().Get("m")
+		if moo == "" {
+			moo = defaultMoosage
+		}
+		say, err := cowsay.Say(cowsay.Phrase(moo), randomCowType(),	cowsay.Thinking())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintln(w, say)
+
+		wenv := os.Getenv(envCowwebWaitMillisecondKey)
+		wms, err := strconv.ParseInt(wenv, 10, 0)
+		if err != nil {
+			wms = 5
+		}
+		ctx, _ := context.WithTimeout(context.Background(), time.Duration(wms) * time.Millisecond)
+		waitWithLoad(ctx)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
