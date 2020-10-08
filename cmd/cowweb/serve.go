8f6fb3e8a097c8e320e1b45344fb9bdd0a719eb4
Hiroshi Hayakawa <hhiroshell@gmail.com>
2020-10-08 20:28:27 +0900

    add graceful shutdown switch

--- cmd/cowweb/serve.go
+++ cmd/cowweb/serve.go
@@ -18,12 +18,14 @@ import (
 var port *int
 var slow *bool
 var load *int
+var shutdownGracefully *bool
 
 func init() {
 	rootCmd.AddCommand(serve)
 	port = serve.Flags().IntP("port", "p", 8080, "Port number for the cowweb http server (default: 8080)")
 	slow = serve.Flags().BoolP("slow", "s", false, "Start the cowweb http server with slow mode")
 	load = serve.Flags().IntP("load", "l", 1024, "CPU load in slow mode")
+	shutdownGracefully = serve.Flags().BoolP("shutdown-gracefully", "g", true, "Gracefully shutting down a cowweb http server")
 }
 
 var serve = &cobra.Command{
@@ -42,14 +44,16 @@ var serve = &cobra.Command{
 			log.Fatal(error)
 		}
 
-		sig := make(chan os.Signal)
-		signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
-		go func() {
-			defer close(sig)
-			<-sig
-			ctx, _ := context.WithTimeout(context.Background(), time.Second*30)
-			server.Shutdown(ctx)
-		}()
+		if *shutdownGracefully {
+			sig := make(chan os.Signal)
+			signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+			go func() {
+				defer close(sig)
+				<-sig
+				ctx, _ := context.WithTimeout(context.Background(), time.Second*30)
+				server.Shutdown(ctx)
+			}()
+		}
 		log.Fatal(server.ListenAndServe())
 	},
 }
